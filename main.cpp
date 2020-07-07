af70a647b52bcf33ee5e044cf93a8f1d0d81a6bb
Maksim Piriyev <maksimpiriyev@gmail.com>
2020-07-07 14:51:02 +0400

    2nd commit

--- main.cpp
+++ main.cpp
@@ -1,5 +1,11 @@
 #include <stdio.h>
 #include "serial_port.h"
+
+void swap(uint8_t& a,uint8_t& b){
+    uint8_t c = b;
+    b = a;
+    a = c;
+}
 int main(int argc,char** argv) {
     if(argc != 4){
         printf("./ZoonUartUpload /dev/ttyYourSerialPort baudRate filePath\n");
@@ -15,12 +21,23 @@ int main(int argc,char** argv) {
         uint_t bytes[4];
         int value;
     }length;
-    fseel(file,0L,SEEK_END);
+    bool isLittleEndian = true;
+    length.value = 1;
+    if(length.bytes[0] != 1){
+        isLittleEndian = false;
+    }
+
+    fseek(file,0L,SEEK_END);
     length.value = ftell(file);
+    if(!isLittleEndian){
+        swap(length.bytes[0],length.bytes[3]);
+        swap(length.bytes[1],length.bytes[2]);
+    }
     fseel(file,0L,SEEK_SET);
     serial.write((uint8_t*)"BOOTLOADX",strlen("BOOTLOADX"));
     sleep(1);
     serial.write(length.bytes,sizeof(length));
+    uint8_t buffer[1024];
     while( (l=fread(buffer,1,1024,file)) > 0){
         serial.write(buffer,l);
     }
